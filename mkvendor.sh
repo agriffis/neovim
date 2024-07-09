680896f8eb832092db79e46c10bef72f638f39f2
Aron Griffis <aron@scampersand.com>
2024-07-09 08:17:37 -0400

    don't bother with vendor tarball

--- mkvendor.sh
+++ mkvendor.sh
@@ -3,11 +3,13 @@
 set -euo pipefail
 
 main() {
+  rm -rf .deps
+  cmake -S cmake.deps -B .deps -G Ninja -DUSE_BUNDLED=OFF -DUSE_BUNDLED_TS_PARSERS=ON
+  targets=$(basename -a .deps/build/downloads/treesitter_* | xargs -I {} echo build/src/{}-stamp/{}-download)
+  ninja -C .deps ${targets}
   rm -rf vendor
-  cmake -S cmake.deps -B vendor -G Ninja -DUSE_BUNDLED=OFF -DUSE_BUNDLED_TS_PARSERS=ON
-  targets=$(basename -a vendor/build/downloads/treesitter_* | xargs -I {} echo build/src/{}-stamp/{}-download)
-  ninja -C vendor ${targets}
-  tar czf vendor.tar.gz -H pax --numeric-owner --owner=0:0 vendor/build/downloads
+  mkdir -p vendor/build
+  cp -a --reflink=always .deps/build/downloads vendor/build
   exit # https://arongriffis.com/2023-11-18-bash-main
 }
 
